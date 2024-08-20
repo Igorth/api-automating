3d7c42731abfa761fb95c125acbb4bcb4005df34
igord <igordiasth@gmail.com>
2024-08-20 10:08:30 -0400

    add test_get_post_by_id `test_script.py`

--- tests/test_script.py
+++ tests/test_script.py
@@ -12,5 +12,13 @@ def test_get_all_posts():
         print(f"GET /posts - Response JSON: {response.json()[:1]}")
 
 
+def test_get_post_by_id():
+    response = client.get("posts/1")
+    if response:
+        print(f"GET /posts/1 - Status Code: {response.status_code}")
+        print(f"GET /posts/1 - Response JSON: {response.json()}")
+
+
 if __name__ == "__main__":
     test_get_all_posts()
+    test_get_post_by_id()
